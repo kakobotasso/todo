11806322260cb8a21721e5591bd2497d31c58881
Kako Botasso <kakodev@gmail.com>
2013-03-28 00:12:17 -0300

    Adicionando views ao projeto

--- core/views.py
+++ core/views.py
@@ -1,1 +1,6 @@
-# Create your views here.
+from models import todo
+from django.shortcuts import render_to_response
+
+def index(request):
+	items = todo.objects.all()
+	return render_to_response('index.html', {'items': items})
