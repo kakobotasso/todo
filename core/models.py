d57c8e1cba10f5f6cb335eca1493bd69975ca44d
Kako Botasso <kakodev@gmail.com>
2013-03-27 23:36:24 -0300

    Criando models e banco de dados

--- core/models.py
+++ core/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class todo(models.Model):
+	name = models.CharField(max_length=100, unique=True)
+	description = models.TextField()
+	created = models.DateTimeField()
+
+	def __unicode__(self):
+		return self.name
